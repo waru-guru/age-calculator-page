7e3d94eee97ea9231628ab4b00b19f73c91c132e
waru-guru <lwaruu02@gmail.com>
2024-06-13 21:14:51 +0300

    I fixed the js part and it's now calculating properly

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ let year = date.getFullYear();
 
 const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
+
 function validate(){
     const inputs = document.querySelectorAll("input");
     let validator = true;
@@ -29,41 +33,53 @@ function validate(){
             input.style.borderBlockColor = " hsl(0, 100%, 67%)";
             parent.querySelector("small").innerText = "This field is required.";
             validator = false;
-        }else if(enter_month.value > 12) {
+        } else {
+            input.style.borderBlockColor = "black";
+            parent.querySelector("small").innerText = "";
+        }
+     });
+        
+        if(enter_month.value < 1 || enter_month.value > 12) {
             enter_month.style.borderBlockColor = " hsl(0, 100%, 67%)";
             enter_month.parentElement.querySelector("small").innerText = "Must be a valid month.";
             validator = false; 
-        }else if(enter_day.value > 31) {
-            enter_day.style.borderBlockColor = " hsl(0, 100%, 67%)";
+
+        }
+
+        const maxDays = months[enter_month.value - 1];
+        if (isLeapYear(enter_year.value) && enter_month.value == 2) {
+            maxDays = 29;
+        }
+
+        if (enter_day.value < 1 || enter_day.value > maxDays) {
+            enter_day.style.borderBlockColor = "hsl(0, 100%, 67%)";
             enter_day.parentElement.querySelector("small").innerText = "Must be a valid day.";
             validator = false;
-        }else{
-            input.style.borderBlockColor = "black";
-            parent.querySelector("small").innerText = "";
-            validator = true; 
         }
-        console.log();
         
-    })
+   
     return validator;
 }
 
 function handleSubmit(e){
     e.preventDefault();
     if(validate()){
-        if(enter_day.value > day){
-            day = day + months[month - 1];
-            month = month - 1;
-        }
-        if(enter_month.value > month){
-            month = month + 12;
-            year = year - 1;
+        let d = day - enter_day.value;
+        let m = month - enter_month.value;
+        let y = year - enter_year.value;
+
+        if (d < 0) {
+            m -= 1;
+            d += months[(month - 2 + 12) % 12];
+            if (isLeapYear(year) && month == 3) {
+                d += 1;
+            }
         }
 
-        const d = day - enter_day.value;
-        const m = day - enter_month.value;
-        const y = day - enter_year.value;
-
+        if (m < 0) {
+            y -= 1;
+            m += 12;
+        }
 
         output_day.innerHTML = d;
         output_month.innerHTML = m;
